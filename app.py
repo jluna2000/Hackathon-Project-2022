957c15d8782710c1524e36136e7c8b4cab97b978
jluna2000 <jesseluna2000@gmail.com>
2022-03-05 17:55:44 -0800

    Getting list of ingredients

--- app.py
+++ app.py
@@ -7,5 +7,19 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/data", methods=["POST", "GET"])
+def ingredients():
+    if request.method == "POST":
+        fullInfo = request.get_json()
+        fullInfo = json.loads(fullInfo)
+        stringOfIngredients = ""
+        for i in range(len(fullInfo['product']['ingredients'])):
+            stringOfIngredients = stringOfIngredients + fullInfo['product']['ingredients'][i]['text'] + "\n"
+        print(stringOfIngredients)
+        return str(stringOfIngredients)
+    else:
+        return redirect(url_for("main"))
+
+
 if __name__ == "__main__":
     app.run()
--- static/scripting.js
+++ static/scripting.js
@@ -1,10 +1,24 @@
 function showInfo(product){
-    const xhttp = new XMLHttpRequest();
-    const method = "GET";
-    const url = "https://world.openfoodfacts.org/api/v0/product/" + product + ".json";
-    const async = true;
+    var xhttp = new XMLHttpRequest();
+    var method = "GET";
+    var url = "https://world.openfoodfacts.org/api/v0/product/" + product + ".json";
+    var async = true;
     xhttp.open(method, url, async);
+    xhttp.onload = function(){
+        getIngredients(this.responseText);
+    }
     xhttp.send();
+}
+
+function getIngredients(fullInfo){
+    var xhttp = new XMLHttpRequest();
+    var method = "POST";
+    var url = "http://127.0.0.1:5000/data";
+    var async = true;
+    xhttp.open(method, url, async);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    var dicInfo = fullInfo;
+    xhttp.send(JSON.stringify(dicInfo));
     xhttp.onload = function(){
         document.getElementById("demo").innerHTML = this.responseText;
     }
